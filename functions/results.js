cfb41372c9c022257194af2ea2cb9a072ba0a1f9
perplexedyawdie <sunkwenga@gmail.com>
2021-11-06 19:05:34 -0500

    init

--- functions/results.js
+++ functions/results.js
@@ -1,7 +1,12 @@
 const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
+  const requestBody = JSON.parse(event.body);
+  const height = requestBody.height;
+  const width = requestBody.width;
+  puppeteer.use(StealthPlugin())
   const browser = await puppeteer.launch({
     args: chromium.args,
     executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
@@ -9,28 +14,27 @@ exports.handler = async function(event, context) {
   });
 
   const page = await browser.newPage();
-
-  await page.goto('https://spacejelly.dev/');
-
-  await page.focus('#search-query')
-  await page.keyboard.type('api');
-
-  const results = await page.$$eval('#search-query + div a', (links) => {
-    return links.map(link => {
-      return {
-        text: link.innerText,
-        href: link.href
-      }
-    });
+  await page.setViewport({
+    width: width,
+    height: height,
+    deviceScaleFactor: 1,
   });
+  await page.goto('https://opensea.io/neverexposed');
+  const pageHeight = await page.evaluate(() => {
+    let body = document.body,
+      html = document.documentElement;
 
+      return Math.max(body.scrollHeight, body.offsetHeight,
+      html.clientHeight, html.scrollHeight, html.offsetHeight);
+    
+  });
   await browser.close();
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       status: 'Ok',
-      results
+      pageHeight: pageHeight
     })
   };
 }
